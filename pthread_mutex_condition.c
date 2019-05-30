77cd9e27e81e79c347c4f3bc6c16dfd40139cdbf
theorist17 <theorist17@gmail.com>
2019-05-31 04:14:31 +0900

    done

--- pthread_mutex_condition.c
+++ pthread_mutex_condition.c
@@ -1,54 +1,51 @@
 #include <stdio.h>
 #include <pthread.h>
-
-int buffer[100];
-int count = 0;
-int in = -1, out = -1;
-pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
-pthread_cond_t buffer_has_space = PTHREAD_COND_INITIALIZER;
+int buffer[100], count = 0; // shared variables : cyclic buffer and its usage counter
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER; // init mutex
+pthread_cond_t buffer_has_space = PTHREAD_COND_INITIALIZER; // init condition variables
 pthread_cond_t buffer_has_data = PTHREAD_COND_INITIALIZER;
-void* producer(void *pData);
-void* consumer(void *pData);
+
+void* produce(void *pData)
+{
+	int i, in = -1;
+	for (i=0; i<1000; i++)
+ 	{
+ 		pthread_mutex_lock(&mutex); // lock the mutex
+  		if(count == 100) // if all 100 buffer using
+   			pthread_cond_wait(&buffer_has_space, &mutex); // unlock and block producer
+		in++; in %= 100;
+   		buffer[in] = i;
+		count++;
+  		pthread_cond_signal(&buffer_has_data); // wake consumer
+		printf("Produce data = %d, in = %d, count = %d\n", buffer[in], in, count);
+  		pthread_mutex_unlock(&mutex); // unlock
+	 }
+}
+
+void* consume(void *pData)
+{
+	int i, out = -1;
+ 	for (i=0; i<1000; i++)
+  	{
+   		pthread_mutex_lock(&mutex); // lock the mutex
+    	if(count == 0) // if no buffer using
+     		pthread_cond_wait(&buffer_has_data, &mutex); // unlock and block consumer
+    	out++; out %= 100;
+    	count--;
+    	pthread_cond_signal(&buffer_has_space); // wake producer
+		printf("Consumed data = %d, out = %d, count = %d\n", buffer[out], out, count);
+  		pthread_mutex_unlock(&mutex); // unlock
+  	}	
+}
 
 int main(void)
 {
 	int i;
-	pthread_t threads[2];
-	pthread_create(&threads[0], NULL, producer, NULL);
-	pthread_create(&threads[1], NULL, consumer, NULL);
-	for (i=0; i<2; i++)
-		pthread_join(threads[i], NULL);
+	pthread_t producer, consumer;
+	pthread_create(&producer, NULL, produce, NULL);
+	pthread_create(&consumer, NULL, consume, NULL);
+	pthread_join(producer, NULL);
+	pthread_join(consumer, NULL);
 	return 0;
 }
-void* producer(void *pData)
-{
-	int i;
-	for (i=0; i<1000; i++)
- 	{
- 		pthread_mutex_lock(&mutex);
-  		if(count ==100)
-   			pthread_cond_wait(&buffer_has_space, &mutex);
-		in++; in %= 100;
-   		buffer[in] = i;
-		count++;
-  		pthread_cond_signal(&buffer_has_data);
-		printf("Produce data = %d, in = %d, count = %d\n", buffer[in], in, count);
-  		pthread_mutex_unlock(&mutex);
-	 }
-}
 
-void* consumer(void *pData)
-{
-	int i;
- 	for (i=0; i<1000; i++)
-  	{
-   		pthread_mutex_lock(&mutex);
-    	if(count == 0)
-     		pthread_cond_wait(&buffer_has_data, &mutex);
-    	out++; out %= 100;
-    	count--;
-    	pthread_cond_signal(&buffer_has_space);
-		printf("Consumed data = %d, out = %d, count = %d\n", buffer[out], out, count);
-  		pthread_mutex_unlock(&mutex);
-  	}	
-}
--- pthread_mutex_semaphore.c
+++ pthread_mutex_semaphore.c
@@ -2,65 +2,53 @@
 #include <pthread.h>
 #include <semaphore.h>
 
-int buffer[100];
-int count = 0;
-int in = -1, out = -1;
-sem_t full, empty, mutex;
+int buffer[100], count = 0; // shared variables : cyclic buffer & its usage counter
+sem_t full, empty, mutex; // semaphores declared among thread, hence shared variables
 
-//pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
-void* producer(void *pData);
-void* consumer(void *pData);
-
-int main(void)
+void* produce(void *pData)
 {
-	int i;
- 	sem_init(&full, 0, 0);
- 	sem_init(&empty, 0, 100);
-	sem_init(&mutex, 0, 1);
-
- 	pthread_t threads[2];
- 	pthread_create(&threads[0], NULL, producer, NULL);
- 	pthread_create(&threads[1], NULL, consumer, NULL);
- 	for (i=0; i<2; i++)
-  		pthread_join(threads[i], NULL);
- 	return 0;
-}
-
-void* producer(void *pData)
-{
- 	int i;
+ 	int i, in = -1;
  	for (i=0; i<1000; i++)
  	{
-  		//pthread_mutex_lock(&mutex);
-  		sem_wait(&empty);
-		sem_wait(&mutex);
+  		sem_wait(&empty); // producer waits on an empty buffer (woken by a consume)
+		sem_wait(&mutex); // without locking the mutex (prevent blocking with resource allocation)
    		in++; in %= 100;
    		buffer[in] = i;
   		count++;
-  		printf("Produce data = %d, count = %d\n", buffer[in], count);
-		sem_post(&mutex);
-  		sem_post(&full);
-  		//pthread_mutex_unlock(&mutex);
- 
+  		printf("Produced data = %d, count = %d\n", buffer[in], count);
+		sem_post(&mutex); // unlock the mutex first (so consumer can have it right after wake)
+  		sem_post(&full); // then wake consumer
  	}
 	return NULL;
 }
-void* consumer(void *pData)
+
+void* consume(void *pData)
 {
-  	int i,data;
+  	int i, out = -1;
   	for (i=0; i<1000; i++)
   	{
-   		//pthread_mutex_lock(&mutex);
-   		sem_wait(&full);
-		sem_wait(&mutex);
+   		sem_wait(&full); // consumer waits on a full buffer (woken by a new produce)
+		sem_wait(&mutex); // without locking the mutex (prevent blocking with resource allocation)
     	out++; out %= 100;
-    	data = buffer[out];
    		count--;
-  		printf("Consume data = %d, count = %d\n", buffer[out], count);
-   		sem_post(&mutex);
-		sem_post(&empty);
-   		//pthread_mutex_unlock(&mutex);
+  		printf("Consumed data = %d, count = %d\n", buffer[out], count);
+   		sem_post(&mutex); // unlock the mutex first (so producer can have it right after wake)
+		sem_post(&empty); // then wake producer
   	}
 	return NULL;
+}
+
+int main(void)
+{
+ 	sem_init(&full, 0, 0); // among 100 buffer, none of them are full at the beginning
+ 	sem_init(&empty, 0, 100); // and all 100 of them are empty
+	sem_init(&mutex, 0, 1); // semaphore with initial value 1 is a mutex
+
+ 	pthread_t producer, consumer;
+ 	pthread_create(&producer, NULL, produce, NULL);
+ 	pthread_create(&consumer, NULL, consume, NULL);
+  	pthread_join(producer, NULL);
+	pthread_join(consumer, NULL);
+ 	return 0;
 }
 
